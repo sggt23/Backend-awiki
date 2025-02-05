e4bc942b11d5cefbb647f056ca27af0b937bb3a1
Diana Isabel Cantillo <cantillo.diana.isabel@gmail.com>
2025-02-05 11:50:19 -0600

    Se agregan los archivos gradle en eclipse

---
+++ src/main/java/com/awiki/AwikiApplication.java
@@ -0,0 +1,13 @@
+package com.awiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwikiApplication.class, args);
+	}
+
+}
